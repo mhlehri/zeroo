1701e9604055001bd53a9c548f3fc5ecdc4b9579
mhlehri <lehrimirza101@gmail.com>
2025-02-09 20:55:00 +0600

    fixed total overview responsive issue

--- src/app/(adminLayout)/admin/dashboard/page.tsx
+++ src/app/(adminLayout)/admin/dashboard/page.tsx
@@ -7,16 +7,16 @@ import TotalUserOverview from "@/components/chart/TotalUserOverview";
 export default function Page() {
   return (
     <div className="p-4">
-      <h2 className="mb-3 rounded bg-teal-800 p-3 text-2xl font-bold text-balance text-slate-200">
+      <h2 className="mb-3 rounded bg-teal-800 p-3 text-lg font-bold text-balance text-slate-200 sm:text-xl md:text-2xl">
         Welcome to Zeroo Admin Dashboard
       </h2>
       <div className="space-y-4">
-        <div className="flex gap-4 *:min-w-56 *:first-of-type:bg-amber-200 *:last-of-type:bg-teal-200 *:nth-of-type-[2]:bg-rose-200">
+        <div className="flex flex-wrap gap-4 *:min-w-56 *:first-of-type:bg-amber-200 *:last-of-type:bg-teal-200 *:nth-of-type-[2]:bg-rose-200">
           <TotalProductsOverview />
           <TotalCategoriesOverview />
           <TotalUserOverview />
         </div>
-        <div className="flex gap-4">
+        <div className="flex flex-wrap gap-4">
           <DailyOrdersOverview />
           <DailyOrderPaymentOverview />
         </div>
