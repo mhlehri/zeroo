bbd98db50ed709dc49f80c0e118941cf39d22592
mhlehri <lehrimirza101@gmail.com>
2025-03-01 07:47:19 +0600

    ... navlinks color

--- src/components/layout/navbar/nav-links.tsx
+++ src/components/layout/navbar/nav-links.tsx
@@ -23,23 +23,23 @@ export default function NavLinks({ links }: NavLinksProps) {
   return (
     <>
       <nav className="container py-0 font-medium text-black xl:px-0">
-        <ul className="*:hover:bg-primary flex items-center justify-center *:h-full *:px-4 *:hover:cursor-pointer *:lg:px-7 xl:px-0 *:xl:px-8">
+        <ul className="flex items-center justify-center *:h-full *:px-4 *:hover:cursor-pointer *:hover:bg-black/10 *:lg:px-7 xl:px-0 *:xl:px-8">
           {links?.map((link, index) =>
             link.subCategory ? (
               <li key={`nav-item-${index}`} className="group relative py-3">
-                <div className="text-md flex cursor-pointer items-center gap-2 p-0 uppercase hover:opacity-70">
+                <div className="text-md flex cursor-pointer items-center gap-2 p-0 uppercase">
                   {link.label}{" "}
                   <ChevronDown
                     size={16}
                     className="text-slate-600 group-hover:text-slate-900"
                   />
                 </div>
-                <ul className="bg-primary-400 invisible absolute top-full left-0 z-50 min-w-[200px] opacity-0 shadow-lg transition-all duration-200 ease-in-out group-hover:visible group-hover:opacity-100">
+                <ul className="invisible absolute top-full left-0 z-50 min-w-[200px] bg-white/95 opacity-0 shadow-lg backdrop-blur-3xl transition-all duration-200 ease-in-out group-hover:visible group-hover:opacity-100">
                   {link.subCategory.map((sub, subIndex) => (
                     <li key={`sub-nav-${subIndex}`}>
                       <Link
                         href={sub.href}
-                        className={`hover:bg-accent hover:text-accent-foreground text-md hover:bg-primary block w-full px-4 py-2 transition-colors ${
+                        className={`hover:bg-accent hover:text-accent-foreground text-md block w-full px-4 py-2 transition-colors hover:bg-black/10 ${
                           pathname === sub.href ? "font-semibold" : ""
                         }`}
                       >
@@ -53,7 +53,7 @@ export default function NavLinks({ links }: NavLinksProps) {
               <li key={`nav-item-${index}`}>
                 <Link
                   href={link.href}
-                  className={`text-md hover:bg-primary block h-full py-3 whitespace-nowrap uppercase hover:opacity-70 ${
+                  className={`text-md block h-full py-3 whitespace-nowrap uppercase ${
                     pathname === link.href ? "font-semibold" : ""
                   }`}
                 >
