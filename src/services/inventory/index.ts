88b7f8da7b99803361256a0ebd886841f1b3ecc7
mhlehri <lehrimirza101@gmail.com>
2025-03-03 17:18:46 +0600

    added gettags, getsizes, addtag service (inventory)

---
+++ src/services/inventory/index.ts
@@ -0,0 +1,43 @@
+"use server";
+import ax from "@/lib/axios-instance";
+import axios from "axios";
+import { revalidatePath } from "next/cache";
+import { FieldValues } from "react-hook-form";
+
+export const getTags = async () => {
+  try {
+    const { data } = await ax.get(`/inventory/tag`);
+    console.log(data, "tags from inventory service");
+    return data?.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response.data;
+    }
+    throw error;
+  }
+};
+export const getSizes = async () => {
+  try {
+    const { data } = await ax.get(`/inventory/size`);
+    console.log(data, "tags from inventory service");
+    return data?.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response.data;
+    }
+    throw error;
+  }
+};
+
+export async function addTag(formData: FieldValues) {
+  try {
+    const { data } = await ax.put(`/inventory/tag`, formData);
+    revalidatePath("/");
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response.data;
+    }
+    throw error;
+  }
+}
