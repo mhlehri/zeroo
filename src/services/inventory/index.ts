130efea9f8d918e4e9e17b6c3d2f08f6b3f688df
mhlehri <lehrimirza101@gmail.com>
2025-03-04 11:30:07 +0600

    ...

--- src/services/inventory/index.ts
+++ src/services/inventory/index.ts
@@ -16,18 +16,6 @@ export const getTags = async () => {
     throw error;
   }
 };
-export const getSizes = async () => {
-  try {
-    const { data } = await ax.get(`/inventory/size`);
-    console.log(data, "tags from inventory service");
-    return data?.data;
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      return error.response.data;
-    }
-    throw error;
-  }
-};
 
 export async function addTag(formData: FieldValues) {
   try {
@@ -41,6 +29,32 @@ export async function addTag(formData: FieldValues) {
     throw error;
   }
 }
+
+export async function deleteTag(formData: FieldValues) {
+  try {
+    const { data } = await ax.delete(`/inventory/tag`, formData);
+    revalidatePath("/");
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response.data;
+    }
+    throw error;
+  }
+}
+
+export const getSizes = async () => {
+  try {
+    const { data } = await ax.get(`/inventory/size`);
+    console.log(data, "tags from inventory service");
+    return data?.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response.data;
+    }
+    throw error;
+  }
+};
 
 export async function addSize(formData: FieldValues) {
   try {
@@ -58,19 +72,6 @@ export async function addSize(formData: FieldValues) {
 export async function deleteSize(formData: FieldValues) {
   try {
     const { data } = await ax.delete(`/inventory/size`, formData);
-    revalidatePath("/");
-    return data;
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      return error.response.data;
-    }
-    throw error;
-  }
-}
-
-export async function deleteTag(formData: FieldValues) {
-  try {
-    const { data } = await ax.delete(`/inventory/tag`, formData);
     revalidatePath("/");
     return data;
   } catch (error) {
