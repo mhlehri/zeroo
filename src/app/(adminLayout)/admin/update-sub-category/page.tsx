9038eb192fe0a65712cc972d19601d0971214744
mhlehri <lehrimirza101@gmail.com>
2025-03-04 13:04:09 +0600

    ...

--- src/app/(adminLayout)/admin/update-sub-category/page.tsx
+++ src/app/(adminLayout)/admin/update-sub-category/page.tsx
@@ -2,7 +2,7 @@ import UpdateSubCategoryForm from "@/components/form/update-sub-category-form";
 
 export default function UpdateSubCategoryPage() {
   return (
-    <div className="flex h-full min-h-[60vh] items-center justify-center">
+    <div className="flex h-[70vh] items-center justify-center">
       <UpdateSubCategoryForm />
     </div>
   );
